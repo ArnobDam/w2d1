8e026ec1b9c8e5b8208d49baf96e2430f753a94b
Arnob <arnobdam@gmail.com>
2022-09-19 16:16:04 -0400

    third try

---
+++ notes.rb
@@ -0,0 +1,27 @@
+#Git:
+#Version control system (vcs), keeps tracks of file changes over time
+
+#Github:
+#Online cloud storage for Git repositories
+#Company completely seperate from Git itself
+
+#git init
+#creates a new Git repository at the current directory
+#Never nest Git repos
+#Never create a repo at the root directory
+
+#git status
+#check modifications from last commit
+#checks if file location is already a git repository
+
+#git add .
+#Makes files ready to commit but does not commit them
+#Confirm you're in the top level of your git repository before running
+
+#git commit -m *message*
+#works in tandem with #git add .
+#commits all files in git repository
+
+#git push 
+#'pushes' changes from your local computer to GitHub
+#only need to do this once a day
